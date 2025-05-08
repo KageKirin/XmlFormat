5fb978e0dd901c0dfc5c6c2c6082b2b194fc9363
Christian Helmich <kagekirin@gmail.com>
2025-05-08 10:24:10 +0900

    feat: add XmlFormat.Tool project (feat/tool-project) [#3]
    - **feat: add XmlFormat.sln**
    - **feat: add XmlFormat.Tool**
    - **feat: add Directory.Build.props**
    - **feat: add Directory.Build.targets**
    - **feat: add Directory.Packages.props**
    - **feat: add NuGet.config**
    - **feat: add makefile**
    - **build: add global package reference to DotNet.ReproducibleBuilds v1.2.25**
    - **build: add global package reference to Microsoft.SourceLink.GitHub v8.0.0**
    - **build: add global package reference to MinVer v6.0.0**
    - **build: add package reference to TurboXml v2.0.2**
    - **build: add package reference to CommandLineParser v2.9.1**
    - **feat: implement file passthrough operation in XmlFormat.Tool**

---
+++ XmlFormat.Tool/Program.cs
@@ -0,0 +1,67 @@
+using System.Diagnostics;
+using CommandLine;
+
+namespace XmlFormat.Tool;
+
+public class Program
+{
+    public class Options
+    {
+        [Option('i', "inline", Required = false, HelpText = "Process input file inline.")]
+        public bool Inline { get; set; }
+
+        [Value(0, MetaName = "input", HelpText = "Input file.")]
+        public string? InputFile { get; set; }
+    }
+
+    public static void Main(string[] args)
+    {
+        CommandLine
+            .Parser.Default.ParseArguments<Options>(args) //
+            .WithParsed(RunOptions)
+            .WithNotParsed(HandleParseError);
+    }
+
+    static void RunOptions(Options options)
+    {
+        Console.WriteLine($"options.Inline: {options.Inline}");
+        Console.WriteLine($"options.InputFile: {options.InputFile}");
+
+        using var istream = OpenInputStreamOrStdIn(options);
+        using var ostream = OpenOutputStreamOrStdOut(options);
+        istream.CopyTo(ostream);
+    }
+
+    static void HandleParseError(IEnumerable<Error> errs)
+    {
+        //handle errors
+    }
+
+    static Stream OpenInputStreamOrStdIn(Options options)
+    {
+        if (string.IsNullOrEmpty(options.InputFile))
+        {
+            if (options.Inline)
+            {
+                throw new InvalidOperationException("option -i/--inline requires an input file");
+            }
+
+            MemoryStream stream = new();
+            Console.OpenStandardInput().CopyTo(stream);
+            stream.Position = 0;
+            return stream;
+        }
+
+        return File.Open(options.InputFile, FileMode.Open, FileAccess.Read);
+    }
+
+    static Stream OpenOutputStreamOrStdOut(Options options)
+    {
+        if (string.IsNullOrEmpty(options.InputFile))
+        {
+            return Console.OpenStandardOutput();
+        }
+
+        return File.Open(options.InputFile + ".tmp", FileMode.Create, FileAccess.Write);
+    }
+}

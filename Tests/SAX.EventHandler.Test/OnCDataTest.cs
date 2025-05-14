c81ae99f6bbeecfd5300b07c1bb43d173b6d4aa2
KageKirin <kagekirin@gmail.com>
2025-05-14 14:40:18 +0900

    feat: implement unit tests for CData callbacks

---
+++ Tests/SAX.EventHandler.Test/OnCDataTest.cs
@@ -0,0 +1,31 @@
+using XmlFormat.SAX;
+
+namespace SAX.EventHandler.Test;
+
+public class OnCDataTest
+{
+    [Theory]
+    [InlineData("<![CDATA[]]>", "")]
+    [InlineData("<![CDATA[ ]]>", " ")]
+    [InlineData("<![CDATA[\n]]>", "\n")]
+    [InlineData("<![CDATA[foobar]]>", "foobar")]
+    [InlineData("<![CDATA[foobar ]]>", "foobar ")]
+    [InlineData("<![CDATA[ foobar]]>", " foobar")]
+    [InlineData("<![CDATA[ foobar ]]>", " foobar ")]
+    [InlineData("<![CDATA[\nfoobar\nhoge\n]]>", "\nfoobar\nhoge\n")]
+    [InlineData("<![CDATA[\nfoobar\nhoge\n ]]>", "\nfoobar\nhoge\n ")]
+    [InlineData("<![CDATA[ \nfoobar\nhoge\n]]>", " \nfoobar\nhoge\n")]
+    [InlineData("<![CDATA[ \nfoobar\nhoge\n ]]>", " \nfoobar\nhoge\n ")]
+    public void MatchOnCallback(string input, string expected)
+    {
+        DelegateXMLEventHandler handler =
+            new()
+            {
+                OnCDataCallback = cdata =>
+                {
+                    Assert.Equal(expected, cdata);
+                }
+            };
+        SaxParser.Parse(input, handler);
+    }
+}

079252aa64f864f0c2878de2fb44bab0c127a922
KageKirin <kagekirin@gmail.com>
2025-05-19 13:53:16 +0900

    feat: implement unit tests for ProcessingInstruction callbacks

---
+++ Tests/SAX.EventHandler.Test/OnProcessingInstructionTest.cs
@@ -0,0 +1,44 @@
+using Superpower.Parsers;
+using XmlFormat.SAX;
+
+namespace SAX.EventHandler.Test;
+
+public class OnProcessingInstructionTest
+{
+    [Theory]
+    //[InlineData("<?php?>", "php")]
+    [InlineData("<?php ?>", "php")]
+    //[InlineData("<?pi?>", "pi")]
+    [InlineData("<?pi ?>", "pi")]
+    public void MatchOnCallbackEmptyContents(string input, string expected)
+    {
+        DelegateXMLEventHandler handler =
+            new()
+            {
+                OnProcessingInstructionCallback = (identifier, contents) =>
+                {
+                    Assert.Equal(expected, identifier);
+                    Assert.NotNull(contents);
+                }
+            };
+        SaxParser.Parse(input, handler);
+    }
+
+    [Theory]
+    [InlineData(@"<?php print(""hello world"");?>", "php", @"print(""hello world"");")]
+    [InlineData(@"<?php print(""hello world""); ?>", "php", @"print(""hello world"");")]
+    public void MatchOnCallback(string input, string expectedIdentifier, string expectedContents)
+    {
+        DelegateXMLEventHandler handler =
+            new()
+            {
+                OnProcessingInstructionCallback = (identifier, contents) =>
+                {
+                    Assert.Equal(expectedIdentifier, identifier);
+                    Assert.NotNull(contents);
+                    Assert.Equal(expectedContents, contents.Trim());
+                }
+            };
+        SaxParser.Parse(input, handler);
+    }
+}

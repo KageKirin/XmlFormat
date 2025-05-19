bbc1323cfc92c745358dfec96e3bf69f011fa322
KageKirin <kagekirin@gmail.com>
2025-05-19 11:00:30 +0900

    feat: implement unit tests for ElementEnd callbacks

---
+++ Tests/SAX.EventHandler.Test/OnElementEndTest.cs
@@ -0,0 +1,47 @@
+using XmlFormat.SAX;
+
+namespace SAX.EventHandler.Test;
+
+public class OnElementEndTest
+{
+    [Theory]
+    [InlineData("</element>", "element")]
+    [InlineData("</el-ement>", "el-ement")]
+    [InlineData("</el_ement>", "el_ement")]
+    [InlineData("</el:ement>", "el:ement")]
+    [InlineData("</el:em_ent>", "el:em_ent")]
+    [InlineData("</el-em_ent>", "el-em_ent")]
+    [InlineData("</el:em-ent>", "el:em-ent")]
+    [InlineData("</el:em-en_t>", "el:em-en_t")]
+    [InlineData("</element1>", "element1")]
+    [InlineData("</element >", "element")]
+    [InlineData("</el-ement >", "el-ement")]
+    [InlineData("</el_ement >", "el_ement")]
+    [InlineData("</el:ement >", "el:ement")]
+    [InlineData("</el:em_ent >", "el:em_ent")]
+    [InlineData("</el-em_ent >", "el-em_ent")]
+    [InlineData("</el:em-ent >", "el:em-ent")]
+    [InlineData("</el:em-en_t >", "el:em-en_t")]
+    [InlineData("</element1 >", "element1")]
+    [InlineData("</element\n >", "element")]
+    [InlineData("</el-ement\n >", "el-ement")]
+    [InlineData("</el_ement\n >", "el_ement")]
+    [InlineData("</el:ement\n >", "el:ement")]
+    [InlineData("</el:em_ent\n >", "el:em_ent")]
+    [InlineData("</el-em_ent\n >", "el-em_ent")]
+    [InlineData("</el:em-ent\n >", "el:em-ent")]
+    [InlineData("</el:em-en_t\n >", "el:em-en_t")]
+    [InlineData("</element1\n >", "element1")]
+    public void MatchOnCallback(string input, string expected)
+    {
+        DelegateXMLEventHandler handler =
+            new()
+            {
+                OnElementEndCallback = cdata =>
+                {
+                    Assert.Equal(expected, cdata);
+                }
+            };
+        SaxParser.Parse(input, handler);
+    }
+}

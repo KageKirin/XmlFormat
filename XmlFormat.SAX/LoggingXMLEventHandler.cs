d9208bfeed293851e4ab8246533165b722308c92
KageKirin <kagekirin@gmail.com>
2025-05-20 15:32:32 +0900

    refactor: adapt LoggingXMLEventHandler to interface method signature changes

--- XmlFormat.SAX/LoggingXMLEventHandler.cs
+++ XmlFormat.SAX/LoggingXMLEventHandler.cs
@@ -7,26 +7,43 @@ namespace XmlFormat.SAX;
 ///</summary>
 public class LoggingXMLEventHandler : IXMLEventHandler
 {
-    public virtual void OnXmlDeclaration(ReadOnlySpan<char> version, ReadOnlySpan<char> encoding, ReadOnlySpan<char> standalone) =>
-        Console.WriteLine($"XML Declaration {version.ToString()}, {encoding.ToString()}, {standalone.ToString()}");
+    public virtual void OnXmlDeclaration(
+        ReadOnlySpan<char> version,
+        ReadOnlySpan<char> encoding,
+        ReadOnlySpan<char> standalone,
+        int line,
+        int column
+    ) => Console.WriteLine($"XML Declaration {version.ToString()}, {encoding.ToString()}, {standalone.ToString()}, {line}:{column}");
 
-    public virtual void OnElementStart(ReadOnlySpan<char> name) => Console.WriteLine($"XML Element Start `{name.ToString()}`");
+    public virtual void OnElementStart(ReadOnlySpan<char> name, int line, int column) =>
+        Console.WriteLine($"XML Element Start `{name.ToString()}`, {line}:{column}");
 
-    public virtual void OnElementEnd(ReadOnlySpan<char> name) => Console.WriteLine($"XML Element End `{name.ToString()}`");
+    public virtual void OnElementEnd(ReadOnlySpan<char> name, int line, int column) =>
+        Console.WriteLine($"XML Element End `{name.ToString()}`, {line}:{column}");
 
-    public virtual void OnElementEmpty(ReadOnlySpan<char> name) => Console.WriteLine($"XML Element Empty `{name.ToString()}`");
+    public virtual void OnElementEmpty(ReadOnlySpan<char> name, int line, int column) =>
+        Console.WriteLine($"XML Element Empty `{name.ToString()}`, {line}:{column}");
 
-    public virtual void OnAttribute(ReadOnlySpan<char> name, ReadOnlySpan<char> value) =>
-        Console.WriteLine($"XML Attribute `{name.ToString()}`:{value.ToString()}");
+    public virtual void OnAttribute(
+        ReadOnlySpan<char> name,
+        ReadOnlySpan<char> value,
+        int nameLine,
+        int nameColumn,
+        int valueLine,
+        int valueColumn
+    ) => Console.WriteLine($"XML Attribute `{name.ToString()}`:{value.ToString()}, {nameLine}:{nameColumn}, {valueLine}:{valueColumn}");
 
-    public virtual void OnProcessingInstruction(ReadOnlySpan<char> identifier, ReadOnlySpan<char> contents) =>
-        Console.WriteLine($"XML processing instruction `{identifier.ToString()}`: `{contents.ToString()}`");
+    public virtual void OnProcessingInstruction(ReadOnlySpan<char> identifier, ReadOnlySpan<char> contents, int line, int column) =>
+        Console.WriteLine($"XML processing instruction `{identifier.ToString()}`: `{contents.ToString()}`, {line}:{column}");
 
-    public virtual void OnCData(ReadOnlySpan<char> cdata) => Console.WriteLine($"XML CData `{cdata.ToString()}`");
+    public virtual void OnCData(ReadOnlySpan<char> cdata, int line, int column) =>
+        Console.WriteLine($"XML CData `{cdata.ToString()}`, {line}:{column}");
 
-    public virtual void OnComment(ReadOnlySpan<char> comment) => Console.WriteLine($"XML Comment `{comment.ToString()}`");
+    public virtual void OnComment(ReadOnlySpan<char> comment, int line, int column) =>
+        Console.WriteLine($"XML Comment `{comment.ToString()}`, {line}:{column}");
 
-    public virtual void OnText(ReadOnlySpan<char> text) => Console.WriteLine($"XML Content {text.ToString()}");
+    public virtual void OnText(ReadOnlySpan<char> text, int line, int column) =>
+        Console.WriteLine($"XML Content {text.ToString()}, {line}:{column}");
 
-    public virtual void OnError(ReadOnlySpan<char> message) => Console.Error.WriteLine($"XML Error: {message.ToString()}");
+    public virtual void OnError(string message, int line, int column) => Console.Error.WriteLine($"XML Error: {message} {line}:{column}");
 }

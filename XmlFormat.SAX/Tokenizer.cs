05e91147a471525ab4dd16f627f14d6d5d047e46
KageKirin <kagekirin@gmail.com>
2025-05-21 15:02:18 +0900

    refactor: improve XmlElementStart parser

--- XmlFormat.SAX/Tokenizer.cs
+++ XmlFormat.SAX/Tokenizer.cs
@@ -134,9 +134,9 @@ public static class XmlTokenizer
     /// </summary>
     static TextParser<Unit> XmlElementStart { get; } =
         from open in Character.EqualTo('<').Try()
-        from identifier in Character.LetterOrDigit.AtLeastOnce().Value(Unit.Value).Try()
-        from rest in Character.Except('>').Many().Value(Unit.Value).Try()
-        from close in Character.EqualTo('>').Try()
+        from identifier in XmlChars.Value(Unit.Value).Try()
+        from attributes in Span.WhiteSpace.IgnoreThen(XmlAttribute).Many().Try()
+        from close in Character.WhiteSpace.Many().IgnoreThen(Character.EqualTo('>')).Try()
         select Unit.Value;
 
     /// <summary>

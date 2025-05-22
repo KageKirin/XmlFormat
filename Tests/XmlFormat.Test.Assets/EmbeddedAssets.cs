0b1379867fcf2e2c355d413212e9f7c401a23a6c
KageKirin <kagekirin@gmail.com>
2025-05-23 01:24:41 +0900

    feat: implement EmbeddedAssets.GetEmbeddedResourceStream() wrapper method to retrieve embedded resource

---
+++ Tests/XmlFormat.Test.Assets/EmbeddedAssets.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+
+namespace XmlFormat.Test.Assets;
+
+public static class EmbeddedAssets
+{
+    internal static string AsString(this Stream stream)
+    {
+        using var source = new StreamReader(stream);
+        var fileContent = source.ReadToEnd();
+        return fileContent;
+    }
+
+    public static Stream? GetEmbeddedResourceStream(string resourceName)
+    {
+        var asm = Assembly.GetExecutingAssembly();
+        Assert.NotNull(asm);
+        if (asm is not null)
+        {
+            var resourceNames = asm.GetManifestResourceNames();
+
+            Assert.Contains(resourceName, resourceNames);
+
+            return asm.GetManifestResourceStream(resourceName);
+        }
+
+        return null;
+    }
+}

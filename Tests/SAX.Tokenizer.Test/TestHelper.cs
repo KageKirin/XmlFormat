af91dfde0cba1a6c0268c3f22132d44092164898
KageKirin <kagekirin@gmail.com>
2025-05-23 10:50:26 +0900

    feat: extend TestHelper with .Tokenize checking Token Type and Value

--- Tests/SAX.Tokenizer.Test/TestHelper.cs
+++ Tests/SAX.Tokenizer.Test/TestHelper.cs
@@ -27,4 +27,33 @@ public static class TestHelper
             return false;
         }
     }
+
+    public record struct TokenTypeAndValue(XmlTokenizer.XmlToken Token, string Value);
+
+    public static bool Tokenize(string input, TokenTypeAndValue[] expectedTokens)
+    {
+        Assert.NotNull(input);
+        Assert.NotEmpty(input);
+
+        try
+        {
+            var tokens = XmlTokenizer.Instance.Tokenize(input);
+            Assert.Equal(expectedTokens.Length, tokens.Count());
+            for (int i = 0; i < expectedTokens.Length; i++)
+            {
+                var token = tokens.ElementAt(i);
+                Assert.True(token.HasValue);
+                Assert.Equal(expectedTokens[i].Token, token.Kind);
+                Assert.Equal(expectedTokens[i].Value, token.Span.ToStringValue());
+                Console.WriteLine($"{token.Span.Position.Line}:{token.Span.Position.Column} {token.Kind} '{token.Span.ToStringValue()}'");
+            }
+
+            return expectedTokens.Length == tokens.Count();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"exception: {ex}");
+            return false;
+        }
+    }
 }

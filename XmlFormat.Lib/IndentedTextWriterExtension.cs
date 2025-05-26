5c7de6f8ca79161b433ba2368b861861aa8acb1b
KageKirin <kagekirin@gmail.com>
2025-05-26 13:43:49 +0900

    feat: implement IndentedTextWriter.WriteNoTabs() extension method

--- XmlFormat.Lib/IndentedTextWriterExtension.cs
+++ XmlFormat.Lib/IndentedTextWriterExtension.cs
@@ -6,6 +6,14 @@ public static class IndentedTextWriterExtension
 {
     public static void WriteTabs(this IndentedTextWriter writer) => writer.Write("");
 
+    public static void WriteNoTabs(this IndentedTextWriter writer, string s)
+    {
+        var indent = writer.Indent;
+        writer.Indent = 0;
+        writer.Write(s);
+        writer.Indent = indent;
+    }
+
     public static void WriteLine(this IndentedTextWriter writer) => writer.WriteLineNoTabs("");
 
     public static void WriteLineNoTabs(this IndentedTextWriter writer) => writer.WriteLineNoTabs("");

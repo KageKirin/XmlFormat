9fa527d482b384004883c32c8bedbfa365c850cc
KageKirin <kagekirin@gmail.com>
2025-05-26 13:43:17 +0900

    feat: implement IndentedTextWriter.WriteLine() extension method taking no arguments

--- XmlFormat.Lib/IndentedTextWriterExtension.cs
+++ XmlFormat.Lib/IndentedTextWriterExtension.cs
@@ -5,4 +5,6 @@ namespace XmlFormat;
 public static class IndentedTextWriterExtension
 {
     public static void WriteTabs(this IndentedTextWriter writer) => writer.Write("");
+
+    public static void WriteLine(this IndentedTextWriter writer) => writer.WriteLineNoTabs("");
 }

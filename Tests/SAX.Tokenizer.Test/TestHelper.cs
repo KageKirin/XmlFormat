62acdb08e803f4a12ec97fa676ff9bc239c0b323
KageKirin <kagekirin@gmail.com>
2025-06-04 15:15:32 +0900

    feat: implement TestHelper.FailTokenize() to assert that Tokenizer does throw an exception
    when fed failing input.
    
    This is part of the Negative Tokenizer tests

--- Tests/SAX.Tokenizer.Test/TestHelper.cs
+++ Tests/SAX.Tokenizer.Test/TestHelper.cs
@@ -74,4 +74,67 @@ public static class TestHelper
             return false;
         }
     }
+
+    public static bool FailTokenize(string input, XmlTokenizer.XmlToken[] expectedTypes)
+    {
+        Assert.NotNull(input);
+        Assert.NotEmpty(input);
+
+        Assert.ThrowsAny<Superpower.ParseException>(() =>
+        {
+            XmlTokenizer.Instance.Tokenize(input);
+        });
+
+        try
+        {
+            var tokens = XmlTokenizer.Instance.Tokenize(input);
+            Assert.Equal(expectedTypes.Length, tokens.Count());
+            for (int i = 0; i < expectedTypes.Length; i++)
+            {
+                var token = tokens.ElementAt(i);
+                Assert.True(token.HasValue);
+                Assert.Equal(expectedTypes[i], token.Kind);
+                Console.WriteLine($"{token.Span.Position.Line}:{token.Span.Position.Column} {token.Kind} '{token.Span.ToStringValue()}'");
+            }
+
+            return expectedTypes.Length == tokens.Count();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"exception: {ex}");
+            return false;
+        }
+    }
+
+    public static bool FailTokenize(string input, TokenTypeAndValue[] expectedTokens)
+    {
+        Assert.NotNull(input);
+        Assert.NotEmpty(input);
+
+        Assert.ThrowsAny<Superpower.ParseException>(() =>
+        {
+            XmlTokenizer.Instance.Tokenize(input);
+        });
+
+        try
+        {
+            var tokens = XmlTokenizer.Instance.Tokenize(input);
+            Assert.Equal(expectedTokens.Length, tokens.Count());
+            for (int i = 0; i < expectedTokens.Length; i++)
+            {
+                var token = tokens.ElementAt(i);
+                Assert.True(token.HasValue);
+                Assert.Equal(expectedTokens[i].Token, token.Kind);
+                Assert.Equal(expectedTokens[i].Value, token.Span.ToStringValue());
+                Console.WriteLine($"{token.Span.Position.Line}:{token.Span.Position.Column} {token.Kind} '{token.Span.ToStringValue()}'");
+            }
+
+            return expectedTokens.Length == tokens.Count();
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"exception: {ex}");
+            return false;
+        }
+    }
 }

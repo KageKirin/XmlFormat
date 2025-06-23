ce2c69bdf644b7bebff9e6bcc3c06437b0721cb7
KageKirin <kagekirin@gmail.com>
2025-06-23 14:09:04 +0900

    feat: implement DisplayXmlFormatToolVersion as Task

---
+++ XmlFormat.MsBuild.Task/DisplayXmlFormatToolVersion.cs
@@ -0,0 +1,39 @@
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Diagnostics;
+
+namespace XmlFormat.MsBuild.Task;
+public class DisplayXmlFormatToolVersion : Microsoft.Build.Utilities.Task
+{
+    private bool _Success = true;
+
+    public virtual bool Success
+    {
+        get { return _Success; }
+        set { _Success = value; }
+    }
+
+    public override bool Execute()
+    {
+        Log.LogMessage(MessageImportance.High, "Formatting: Checking `xf` version");
+
+        Process process = new Process();
+        process.StartInfo = new ProcessStartInfo()
+        {
+            FileName = "xf",
+            Arguments = "--version",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        Console.WriteLine(output);
+
+        process.WaitForExit();
+        return Success;
+    }
+}

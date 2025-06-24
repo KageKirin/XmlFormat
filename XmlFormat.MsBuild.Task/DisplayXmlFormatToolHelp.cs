2eb7966bbb7ca2c31f6bc8cadf82eafed70d04f0
KageKirin <kagekirin@gmail.com>
2025-06-24 10:35:17 +0900

    refactor: set and return success depending on process exit code in DisplayXmlFormatToolHelp task

--- XmlFormat.MsBuild.Task/DisplayXmlFormatToolHelp.cs
+++ XmlFormat.MsBuild.Task/DisplayXmlFormatToolHelp.cs
@@ -34,6 +34,7 @@ public class DisplayXmlFormatToolHelp : Microsoft.Build.Utilities.Task
         Console.WriteLine(output);
 
         process.WaitForExit();
+        Success = process.ExitCode == 0;
         return Success;
     }
 }

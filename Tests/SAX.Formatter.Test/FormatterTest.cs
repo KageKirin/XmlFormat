ac6140fd7b9634866292a26c40a52074b247e697
KageKirin <kagekirin@gmail.com>
2025-07-11 15:25:37 +0900

    feat: unit test cases for multi-word text inside element to SAX.Formatter.Test

--- Tests/SAX.Formatter.Test/FormatterTest.cs
+++ Tests/SAX.Formatter.Test/FormatterTest.cs
@@ -25,7 +25,9 @@ public class FormatterTest
     [InlineData("<element /> ", "<element />")]
     [InlineData("<element attribute=\"1\"/> ", "<element attribute=\"1\" />")]
     [InlineData("<element>test</element> ", "<element>test</element>")]
+    [InlineData("<element>test test</element> ", "<element>\ntest test\n</element>")]
     [InlineData("<element attribute=\"1\">test</element> ", "<element attribute=\"1\">test</element>")]
+    [InlineData("<element attribute=\"1\">test test</element> ", "<element attribute=\"1\">\ntest test\n</element>")]
     public void IdentityTest(string input, string expected)
     {
         var formatted = XmlFormat.XmlFormat.Format(input, new FormattingOptions(80, "", 1, 2));
@@ -61,7 +63,9 @@ public class FormatterTest
     [InlineData("<element /> ", "<element />")]
     [InlineData("<element attribute=\"1\"/> ", "<element attribute=\"1\" />")]
     [InlineData("<element>test</element> ", "<element>test</element>")]
+    [InlineData("<element>test test</element> ", "<element>\ntest test\n</element>")]
     [InlineData("<element attribute=\"1\">test</element> ", "<element attribute=\"1\">test</element>")]
+    [InlineData("<element attribute=\"1\">test test</element> ", "<element attribute=\"1\">\ntest test\n</element>")]
     public void IdentityTestStream(string input, string expected)
     {
         Encoding encoding = new UTF8Encoding(true);

d2f4c34def4fcfbddff0820742e517cc82f5cf32
KageKirin <kagekirin@gmail.com>
2025-07-11 13:55:59 +0900

    fix: adjust unit tests to expected results in SAX.Formatter.Test

--- Tests/SAX.Formatter.Test/FormatterTest.cs
+++ Tests/SAX.Formatter.Test/FormatterTest.cs
@@ -24,8 +24,8 @@ public class FormatterTest
     [InlineData("<element/> ", "<element />")]
     [InlineData("<element /> ", "<element />")]
     [InlineData("<element attribute=\"1\"/> ", "<element attribute=\"1\" />")]
-    [InlineData("<element>test</element> ", "<element>\ntest\n</element>")]
-    [InlineData("<element attribute=\"1\">test</element> ", "<element attribute=\"1\">\ntest\n</element>")]
+    [InlineData("<element>test</element> ", "<element>test</element>")]
+    [InlineData("<element attribute=\"1\">test</element> ", "<element attribute=\"1\">test</element>")]
     public void IdentityTest(string input, string expected)
     {
         var formatted = XmlFormat.XmlFormat.Format(input, new FormattingOptions(80, "", 1, 2));
@@ -60,8 +60,8 @@ public class FormatterTest
     [InlineData("<element/> ", "<element />")]
     [InlineData("<element /> ", "<element />")]
     [InlineData("<element attribute=\"1\"/> ", "<element attribute=\"1\" />")]
-    [InlineData("<element>test</element> ", "<element>\ntest\n</element>")]
-    [InlineData("<element attribute=\"1\">test</element> ", "<element attribute=\"1\">\ntest\n</element>")]
+    [InlineData("<element>test</element> ", "<element>test</element>")]
+    [InlineData("<element attribute=\"1\">test</element> ", "<element attribute=\"1\">test</element>")]
     public void IdentityTestStream(string input, string expected)
     {
         Encoding encoding = new UTF8Encoding(true);

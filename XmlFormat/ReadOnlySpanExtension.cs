07b263e42866d8d9eeee7726e7fce166ffee2e40
KageKirin <kagekirin@gmail.com>
2025-07-14 11:10:21 +0900

    feat: implement ReadOnlySpan<char>.IndexOf(predicate, startIndex, count) extension method

--- XmlFormat/ReadOnlySpanExtension.cs
+++ XmlFormat/ReadOnlySpanExtension.cs
@@ -236,4 +236,31 @@ public static class ReadOnlySpanCharExtensions
         }
         return -1;
     }
+
+    /// <summary>
+    /// Finds the zero-based index of the first character in a sub-section of the span that matches the conditions defined by the specified predicate.
+    /// </summary>
+    /// <param name="span">The read-only character span to search.</param>
+    /// <param name="match">The predicate that defines the conditions of the character to search for.</param>
+    /// <param name="startIndex">The zero-based starting index of the search.</param>
+    /// <param name="count">The number of characters in the section to search.</param>
+    /// <returns>
+    /// The zero-based index of the first occurrence of a character that matches the conditions defined by <paramref name="match"/> within the section of the span that starts at <paramref name="startIndex"/> and contains up to <paramref name="count"/> number of characters, if found; otherwise, –1.
+    /// </returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int IndexOf(this ReadOnlySpan<char> span, Func<char, bool> match, int startIndex, int count)
+    {
+        for (int i = startIndex; i < span.Length; i++)
+        {
+            if (match(span[i]))
+            {
+                return i;
+            }
+
+            count--;
+            if (count == 0)
+                break;
+        }
+        return -1;
+    }
 }
